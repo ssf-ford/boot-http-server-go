1ac1b4d4363d151eea84f030a77b9f8438043786
Santawat Santiteerakul <47019880+ssf-ford@users.noreply.github.com>
2026-06-14 19:49:45 +0700

    02 routing (#2)
    * 02-routing: stateful handlers - create request counter
    
    * 02-routing: routing - method based pattern matching

--- main.go
+++ main.go
@@ -8,26 +8,19 @@ import (
 func main() {
 	filePathRoot := "."
 	port := "8080"
+	apiCfg := &apiConfig{}
 
 	mux := http.NewServeMux()
-	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir(filePathRoot))))
-	mux.HandleFunc("/healthz", handlerHealthz)
+	mux.Handle("GET /app/", http.StripPrefix("/app", apiCfg.middlewareMetricsInc(http.FileServer(http.Dir(filePathRoot)))))
+	mux.HandleFunc("GET /healthz", handlerHealthz)
+	mux.HandleFunc("GET /metrics", apiCfg.handlerMetrics)
+	mux.HandleFunc("POST /reset", apiCfg.handlerMetricsReset)
 
-	server := http.Server{
+	server := &http.Server{
 		Addr:    ":" + port,
 		Handler: mux,
 	}
 
 	log.Printf("Serving files from %s on port: %s\n", filePathRoot, port)
 	log.Fatal(server.ListenAndServe())
-}
-
-func handlerHealthz(w http.ResponseWriter, r *http.Request) {
-	if r.URL.Path != "/healthz" {
-		http.Error(w, "Not found", http.StatusNotFound)
-		return
-	}
-	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
-	w.WriteHeader(http.StatusOK)
-	w.Write([]byte(http.StatusText(http.StatusOK)))
 }
