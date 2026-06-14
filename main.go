10ebbb52cef112833ca77993814b820412c054af
Santawat Santiteerakul <santawat.s@gmail.com>
2026-06-14 14:27:23 +0700

    01-servers: custom handlers

---
+++ main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+	filePathRoot := "."
+	port := "8080"
+
+	mux := http.NewServeMux()
+	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir(filePathRoot))))
+	mux.HandleFunc("/healthz", handlerHealthz)
+
+	server := http.Server{
+		Addr:    ":" + port,
+		Handler: mux,
+	}
+
+	log.Printf("Serving files from %s on port: %s\n", filePathRoot, port)
+	log.Fatal(server.ListenAndServe())
+}
+
+func handlerHealthz(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/healthz" {
+		http.Error(w, "Not found", http.StatusNotFound)
+		return
+	}
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(http.StatusText(http.StatusOK)))
+}
